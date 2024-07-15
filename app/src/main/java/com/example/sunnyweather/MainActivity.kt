4b0736f40886dea879bc6d5864ab36955de86d9e
wangbing <wangbing257@hellobike.com>
2024-07-15 17:32:12 +0800

    feat:hava data

--- app/src/main/java/com/example/sunnyweather/MainActivity.kt
+++ app/src/main/java/com/example/sunnyweather/MainActivity.kt
@@ -24,8 +24,6 @@ class MainActivity : ComponentActivity() {
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
 
-
-
                     )
                 }
             }
